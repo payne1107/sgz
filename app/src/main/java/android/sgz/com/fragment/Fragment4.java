964cca79a35c8a44a5fefd805c942e9db52bd468
payne1107 <924579851@qq.com>
2018-05-06 12:21:16 +0800

    123
    123

--- app/src/main/java/android/sgz/com/fragment/Fragment4.java
+++ app/src/main/java/android/sgz/com/fragment/Fragment4.java
@@ -11,13 +11,13 @@ import android.view.ViewGroup;
  * Created by 92457 on 2018/4/16.
  */
 
-public class Fragment4 extends BaseFragment{
+public class Fragment4 extends BaseFragment {
     @Override
     public View onCustomCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         if (mRootView == null) {
             mRootView = inflater.inflate(R.layout.fragment4, null);
         }
-        //缓存的rootView需要判断是否已经被加过parent,如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误。
+        //缓存的rootView需要判断是否已经被加过parent,如果有parent需要从parent删除，要不然会发生这个rootview已经有parent的错误
         ViewGroup parent = (ViewGroup) mRootView.getParent();
         if (parent != null) {
             parent.removeView(mRootView);
